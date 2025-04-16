afd7970e07ec0a0dcb27c55b34f96377b12dfa84
nicolasperin <nicopperin@gmail.com>
2025-04-16 20:51:37 -0300

    lista finalizada

--- lista-de-exercicios-2/exercicio-010.py
+++ lista-de-exercicios-2/exercicio-010.py
@@ -1,2 +1,29 @@
 idade = int(input("Informe a idade do paciente: "))
-peso = int(input("Informe o peso do paciente: "))
+peso = int(input("Informe o peso do paciente em quilos: "))
+
+if idade >= 12:
+    if peso >= 60:
+        dosagem = 1000
+    else:
+        dosagem = 875
+else:
+    if peso > 30:
+        dosagem = 750
+    elif peso > 24:
+        dosagem = 500
+    elif peso > 16:
+        dosagem = 375
+    elif peso > 9:
+        dosagem = 250
+    elif peso >= 5:
+        dosagem = 125
+    else:
+        dosagem = 0
+
+# Uma gota = 25mg 
+
+gotas_necessarias = dosagem / 25
+
+print("O paciente vai precisar de %d gotas" %gotas_necessarias)
+
+
