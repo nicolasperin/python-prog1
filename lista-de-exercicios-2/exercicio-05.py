705a6b9e20f5d042c45869f9a725ebd654f96b75
nicolasperin <nicopperin@gmail.com>
2025-05-07 14:40:17 -0300

    correcao ex5 lista 2

--- lista-de-exercicios-2/exercicio-05.py
+++ lista-de-exercicios-2/exercicio-05.py
@@ -4,7 +4,7 @@ num1 = float(input("Digite o primeiro número: "))
 num2 = float(input("Digite o primeiro segundo: "))
 num3 = float(input("Digite o primeiro terceiro: "))
 
-if num1 > num2 and num1 > num3:
+if num1 >= num2 and num1 >= num3:
     maior = num1
     if num2 > num3:
         meio = num2
@@ -12,7 +12,7 @@ if num1 > num2 and num1 > num3:
     else:
         meio = num3
         menor = num2
-elif num2 > num1 and num2 > num3:
+elif num2 >= num3:
     maior = num2
     if num1 > num3:
         meio = num1
@@ -20,7 +20,7 @@ elif num2 > num1 and num2 > num3:
     else:
         meio = num3
         menor = num1
-elif num3 > num1 and num3 > num2:
+else:
     maior = num3
     if num1 > num2:
         meio = num1
